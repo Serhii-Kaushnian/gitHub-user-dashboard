fac28fc24248616155e72bcf252ca7d388bb07dc
Serhii Kaushnian <skaushnjan@gmail.com>
2023-08-23 15:42:44 +0200

    gh-page

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename='/github-user-dashboard'>
           <App />
         </BrowserRouter>
       </PersistGate>
